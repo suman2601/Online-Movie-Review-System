2764b78ecc8b25ccd05fa0f8a063b6d6091bba0b
suman2601 <68642239+suman2601@users.noreply.github.com>
2021-06-21 14:33:58 +0530

    Update settings.py

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'moviereview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'moviereviewdb'),
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        
     }
 }
 
